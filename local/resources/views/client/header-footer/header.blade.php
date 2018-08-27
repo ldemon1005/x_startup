e0e392ce436760ef0b67d2f2e32c9f5f5f80ebf8
boyvn2911 <boyvn2911@gmail.com>
2018-08-27 18:51:03 +0700

    a

--- local/resources/views/client/header-footer/header.blade.php
+++ local/resources/views/client/header-footer/header.blade.php
@@ -1,19 +1,19 @@
 <div id="header">
     <section>
         <div class="d-flex">
-            <div class="logo"></div>
+            <a href="{{asset('/')}}" class="logo"></a>
             <ul class="menu">
-                <li><a href="">Trang chủ</a></li>
+                <li><a href="{{ asset('/') }}">Trang chủ</a></li>
                 <li><a href="">Tin tức</a></li>
                 <li><a href="">Trang nhóm</a></li>
             </ul>
             <div class="login">
-                <a class="login-btn" href="{{route('login_client')}}">{{\Illuminate\Support\Facades\Auth::user() ? \Illuminate\Support\Facades\Auth::user()->fullname : 'Đăng nhập'}}</a>
-                @if(! \Illuminate\Support\Facades\Auth::user())
-                    <a class="signup-btn" href="{{route('register_client')}}">Đăng ký</a>
+                @if( Auth::guest() )
+                <a class="login-btn" href="{{route('login_client')}}">Đăng nhập</a>
+                <a class="signup-btn" href="{{route('register_client')}}">Đăng ký</a>
+                @else
+                <a class="user">{{Auth::user()->fullname}} <span class="ava" style="background-image: url('');"></span></a>
                 @endif
-
-                <a class="user">Cao T.Linh <span class="ava" style="background-image: url('');"></span></a>
             </div>
         </div>
     </section>
