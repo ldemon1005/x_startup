d24e7c45f1142799bffcddfe11b8ea45a32cbbf1
boyvn2911 <boyvn2911@gmail.com>
2018-08-30 10:57:23 +0700

    Merge branch 'master' of https://github.com/ldemon1005/x_startup into son
    # Conflicts:
    #	local/resources/views/client/header-footer/header.blade.php
    #	local/routes/web.php

--- local/resources/views/client/master-2.blade.php
+++ local/resources/views/client/master-2.blade.php
@@ -3,6 +3,7 @@
 <head>
     <title></title>
     <link rel="stylesheet" type="text/css" href="{{ asset('local/resources/assets/guest/css') }}/reset.css">
+    <link rel="stylesheet" type="text/css" href="{{ asset('local/resources/assets/guest/css') }}/fonts.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="{{ asset('local/resources/assets/guest/css') }}/base.css">
     @yield('css')
